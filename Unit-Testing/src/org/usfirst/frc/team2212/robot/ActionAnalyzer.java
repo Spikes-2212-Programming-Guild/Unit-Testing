de4e9ebcb1495ea41d6ae44253b56957b991567a
Tomer Sitton <tomer.sitton1@gmail.com>
2017-01-26 17:28:36 +0200

    changed parameters from arrays to arraylists

--- Unit-Testing/src/org/usfirst/frc/team2212/robot/ActionAnalyzer.java
+++ Unit-Testing/src/org/usfirst/frc/team2212/robot/ActionAnalyzer.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team2212.robot;
 
+import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -7,38 +8,38 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class ActionAnalyzer {
 
-	
-	public ActionAnalyzer(Joystick[]joysticks, SpeedController []motors){
-		
-		for(Joystick current: joysticks){
+	public ActionAnalyzer(ArrayList<Joystick> joysticks, ArrayList<SpeedController> motors) {
+
+		for (Joystick current : joysticks) {
 			comingInput(current);
 		}
-		
+
 		int currentPort = 0;
-		
-		for(SpeedController current: motors){
+
+		for (SpeedController current : motors) {
 			comingOutput(current, currentPort);
 			currentPort++;
 		}
-		
+
 	}
-	
-	public void comingInput(Joystick joystick){
+
+	public void comingInput(Joystick joystick) {
 		JoystickButton button;
-		for(int i = 0; i< joystick.getButtonCount(); i++){
+		for (int i = 0; i < joystick.getButtonCount(); i++) {
 			button = new JoystickButton(joystick, i);
-			if(button.get()){
-				System.out.println("Button number "+ i +" was pressed.");
+			if (button.get()) {
+				System.out.println("Button number " + i + " was pressed.");
 			}
 		}
-		if(joystick.getX() != 0 || joystick.getY() != 0){
-			System.out.println("The joystick "+ joystick.getName() +" moved on the X axis: "+ joystick.getX() +" and on the Y axis: "+ joystick.getY());
+		if (joystick.getX() != 0 || joystick.getY() != 0) {
+			System.out.println("The joystick " + joystick.getName() + " moved on the X axis: " + joystick.getX()
+					+ " and on the Y axis: " + joystick.getY());
 		}
 	}
-	
-	public void comingOutput(SpeedController controller, int port){
-		if(controller.get()!=0){
-			System.out.println("Motor number "+ port +" moved, speed: "+ controller.get());
+
+	public void comingOutput(SpeedController controller, int port) {
+		if (controller.get() != 0) {
+			System.out.println("Motor number " + port + " moved, speed: " + controller.get());
 		}
 	}
 }
